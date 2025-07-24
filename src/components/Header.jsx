bf0c5c9b49ecc33b183231b66412f79390dcfa28
Edoardo Lova <edoardolova81@gmail.com>
2025-07-24 17:54:30 +0200

    feat: add navbar active

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header(){
     return(
         <>
             <header className="bg-dark">
-                <nav className="nav justify-content-center bg-dark py-3 ">
+                <nav className="nav justify-content-center  py-3 ">
                     <NavLink className="nav-link  text-light" to="/" aria-current="page">Home</NavLink>
                     <NavLink className="nav-link text-light" to="/products">Products</NavLink>
                     <NavLink className="nav-link  text-light" to="/about">About Us</NavLink>
