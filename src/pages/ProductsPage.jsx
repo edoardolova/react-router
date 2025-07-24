52b25494a7d7092683a1bbda4c802f7ca51bc2e5
Edoardo Lova <edoardolova81@gmail.com>
2025-07-24 17:34:07 +0200

    feat: add products card and api

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,7 +1,45 @@
+import React, { useState, useEffect } from 'react';
+
 export default function ProductsPage(){
+     const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            setProducts(data); 
+        })
+        .catch((err) => {
+            console.error(err.message); 
+        });
+    }, []);
+    
     return(
         <>
-            <p>sono la products page</p>
+        <div className="container">
+            <h1 className="mt-5">SCOPRI I NOSTRI PRODOTTI</h1>
+            <div className="row gy-3 py-4">
+                {products.map((product, index)=>{
+                    return(
+                        <>
+                            <div className="col-12 col-md-4">
+                                <div className="card h-100  ">
+                                    <img src={product.image} className="card-img rouded" alt={product.title} />
+                                    <div className="card-body px-1 d-flex flex-column" key={product.id}>
+                                        <h3 className="card-title mb-3">{product.title}</h3>
+                                        <p className="card-text mb-2  fw-medium"><span className="fw-bold me-1 ">Prezzo:</span>{product.price}€</p>
+                                        <p className="card-text mb-2  fw-medium"><span className="fw-bold me-1 ">Categoria:</span>{product.category}</p>
+                                        <p className="card-text mb-2  fw-medium"><span className="fw-bold me-1 ">Descrizione:</span>{product.description}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )
+                })}
+            </div>
+        </div>
         
         </>
     )
