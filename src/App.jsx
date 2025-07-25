634fc4c25f28d2aa03d7717a2a4f45ed5bdfca37
Edoardo Lova <edoardolova81@gmail.com>
2025-07-25 17:35:18 +0200

    feat: add NotFound component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ProductsPage from './pages/ProductsPage';
 import AboutPage from './pages/AboutPage';
 import DefaultLayout from './layouts/DefaultLayout';
 import ProductDetail from './pages/ProductDetail';
+import NotFound from './pages/NotFound';
 function App() {
 
   return (
@@ -17,7 +18,7 @@ function App() {
             <Route path='/products' Component={ProductsPage}/>
             <Route path='/about' Component={AboutPage}/>
             <Route path='/products/:id' Component={ProductDetail}></Route>
-           
+            <Route path='*' Component={NotFound}/>
           </Route>
         </Routes>
       </BrowserRouter>}
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="container py-4">
+      <h1>404 - Pagina non trovata</h1>
+      <p>La pagina che stai cercando non esiste.</p>
+    </div>
+  );
+}
