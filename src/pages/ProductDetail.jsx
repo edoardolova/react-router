2724dfa79597aed31748524bf9604b035bbe5b57
Edoardo Lova <edoardolova81@gmail.com>
2025-07-25 17:44:58 +0200

    feat: add loading to ProductDetail and ProductsPage components

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -7,13 +7,16 @@ export default function ProductDetail(){
     const [product, setProduct] = useState(null)
     const [nextProductExists, setNextProductExists] = useState(true);
     const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(true); 
+    
 
     useEffect(() => {
-        // Recupera il prodotto corrente
+        setIsLoading(true);
         fetch(url)
         .then((res) => res.json())
         .then((data) => {
             setProduct(data);
+            setIsLoading(false);
             
             //check if next exist
             fetch(`https://fakestoreapi.com/products/${parseInt(id) + 1}`)
@@ -40,41 +43,44 @@ export default function ProductDetail(){
     };
     return(
         <>
-            {product &&(
-                <div className="container">
-
-                    <div className="row">
-
-                        <div className="col-12 my-5">
-                            <div className="card border-0">
-                                <div className="row">
-                                    <div className="col-1 d-flex">
-
-                                        <button className="btn border-0 my-auto" onClick={goPrevious}><i class="bi bi-arrow-left-circle-fill fs-3"></i></button>
-                                    </div>
-                                    <div className="col-5  d-flex">
-                                            <img src={product.image} className="img-fluid mx-auto" style={{height:'600px'}} alt={product.title} />
-                                    </div>
-                                    <div className="col-1 d-flex">
-                                            <button className="btn border-0 my-auto" onClick={goNext}><i class="bi bi-arrow-right-circle-fill fs-3"></i></button>
-
-                                    </div>
-                                    <div className="col-5 ">
-                                        <div className="card-body px-1 d-flex flex-column h-100" key={product.id}>
-                                            <h1 className="card-title mb-3">{product.title}</h1>
-                                            <p className="card-text mb-2  fw-medium"><span className="fw-bold me-1 ">Categoria:</span>{product.category}</p>
-                                            <p className="card-text mb-2  fw-medium"><span className="fw-bold me-1 ">Prezzo:</span>{product.price}€</p>
-                                            <p className="card-text mb-2  fw-medium"><span className="fw-bold me-1 ">Descrizione:</span>{product.description}</p>
-                                            <button className="btn btn-primary w-25 mt-4 ">Compra</button>
+            {isLoading? (
+                <h2>Loading</h2>
+            ) : (product &&(
+                    <div className="container">
+    
+                        <div className="row">
+                            <div className="col-12 my-5">
+                                <div className="card border-0">
+                                    <div className="row">
+                                        <div className="col-1 d-flex">
+    
+                                            <button className="btn border-0 my-auto" onClick={goPrevious}><i class="bi bi-arrow-left-circle-fill fs-3"></i></button>
                                         </div>
+                                        <div className="col-5  d-flex">
+                                                <img src={product.image} className="img-fluid mx-auto" style={{height:'600px'}} alt={product.title} />
+                                        </div>
+                                        <div className="col-1 d-flex">
+                                                <button className="btn border-0 my-auto" onClick={goNext}><i class="bi bi-arrow-right-circle-fill fs-3"></i></button>
+    
+                                        </div>
+                                        <div className="col-5 ">
+                                            <div className="card-body px-1 d-flex flex-column h-100" key={product.id}>
+                                                <h1 className="card-title mb-3">{product.title}</h1>
+                                                <p className="card-text mb-2  fw-medium"><span className="fw-bold me-1 ">Categoria:</span>{product.category}</p>
+                                                <p className="card-text mb-2  fw-medium"><span className="fw-bold me-1 ">Prezzo:</span>{product.price}€</p>
+                                                <p className="card-text mb-2  fw-medium"><span className="fw-bold me-1 ">Descrizione:</span>{product.description}</p>
+                                                <button className="btn btn-primary w-25 mt-4 ">Compra</button>
+                                            </div>
+                                        </div>
+    
+    
                                     </div>
-
-
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                )
+
             )}
         </>
     )
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function ProductsPage(){
-     const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true); 
 
     useEffect(() => {
+        setIsLoading(true);
         fetch('https://fakestoreapi.com/products')
         .then((response) => {
             return response.json();
         })
         .then((data) => {
             setProducts(data); 
+            setIsLoading(false); 
         })
         .catch((err) => {
             console.error(err.message); 
@@ -19,28 +22,34 @@ export default function ProductsPage(){
     
     return(
         <>
-        <div className="container">
-            <h1 className="mt-5">SCOPRI I NOSTRI PRODOTTI</h1>
-            <div className="row gy-3 py-4">
-                {products.map((product, index)=>{
-                    return(
-                        <>
-                            <div className="col-12 col-md-4">
-                                <div className="card h-100  ">
-                                    <img src={product.image} className="card-img rouded" alt={product.title} />
-                                    <div className="card-body px-1 d-flex flex-column" key={product.id}>
-                                        <h3 className="card-title mb-3">{product.title}</h3>
-                                        <p className="card-text mb-2  fw-medium"><span className="fw-bold me-1 ">Prezzo:</span>{product.price}€</p>
-                                        <p className="card-text mb-2  fw-medium"><span className="fw-bold me-1 ">Categoria:</span>{product.category}</p>
+        {isLoading ?(
+            <h3>loading</h3>
+        ) :(
+
+            <div className="container">
+                <h1 className="mt-5">SCOPRI I NOSTRI PRODOTTI</h1>
+                <div className="row gy-3 py-4">
+                    {products.map((product, index)=>{
+                        return(
+                            <>
+                                <div className="col-12 col-md-4">
+                                    <div className="card h-100  ">
+                                        <img src={product.image} className="card-img rouded" alt={product.title} />
+                                        <div className="card-body px-1 d-flex flex-column" key={product.id}>
+                                            <h3 className="card-title mb-3">{product.title}</h3>
+                                            <p className="card-text mb-2  fw-medium"><span className="fw-bold me-1 ">Prezzo:</span>{product.price}€</p>
+                                            <p className="card-text mb-2  fw-medium"><span className="fw-bold me-1 ">Categoria:</span>{product.category}</p>
+                                        </div>
+                                        <Link className='btn btn-primary' to={`/products/${product.id}`}>Scopri</Link>
                                     </div>
-                                    <Link className='btn btn-primary' to={`/products/${product.id}`}>Scopri</Link>
                                 </div>
-                            </div>
-                        </>
-                    )
-                })}
+                            </>
+                        )
+                    })}
+                </div>
             </div>
-        </div>
+
+        )}
         
         </>
     )
